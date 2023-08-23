3237c99e2ce2ed7f8b4b418b180651b40866235e
SangHui48 <vodlrm123@naver.com>
2023-08-23 11:08:20 +0900

    homepage layout, onboarding

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        
+    }
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+    return (
+        <div>
+            Account Profile
+        </div>
+    )
+}
+
+export default AccountProfile
